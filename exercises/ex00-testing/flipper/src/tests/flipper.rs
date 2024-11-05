02ad5f6ce443170c3ed928b407f1c5149e1c7a0d
DanieleDiBenedetto <daniele@horizenlabs.io>
2024-11-05 11:21:22 +0100

    Revert "tests: flipper (ex00 done)"
    This reverts commit dd77f9074585e9cd46db6324ae50b8a89ca8e0c3.

--- exercises/ex00-testing/flipper/src/tests/flipper.rs
+++ exercises/ex00-testing/flipper/src/tests/flipper.rs
@@ -1,36 +1,28 @@
 use super::mock::*;
-use frame_support::{assert_noop, assert_ok, assert_err};
-
-type Error = crate::Error::<TestRuntime>;
+use crate::{pallet, Error};
+use frame_support::{assert_noop, assert_ok};
 
 #[test]
 fn set_value_ok() {
 	new_test_ext().execute_with(|| {
-		assert_ok!(Flipper::set_value(RuntimeOrigin::signed(ALICE), false));
+		assert_ok!(Flipper::set_value(Origin::signed(ALICE), false));
 		assert_eq!(Flipper::value(), Some(false));
 	});
 }
 
 #[test]
 fn set_value_err_already_set() {
-	new_test_ext().execute_with(|| {
-		assert_ok!(Flipper::set_value(RuntimeOrigin::signed(ALICE), true));
-		assert_noop!(Flipper::set_value(RuntimeOrigin::signed(ALICE), true), Error::AlreadySet);
-	});
+	new_test_ext().execute_with(|| todo!("Verify if the function returns the expected error."));
 }
 
 #[test]
 fn flip_value_ok() {
-	new_test_ext().execute_with(|| {
-		assert_ok!(Flipper::set_value(RuntimeOrigin::signed(ALICE), false));
-		assert_ok!(Flipper::flip_value(RuntimeOrigin::signed(ALICE)));
-		assert_eq!(Flipper::value(), Some(true));
-	});
+	new_test_ext()
+		.execute_with(|| todo!("Ensure the good behaviour of the flip_value() function."));
 }
 
 #[test]
 fn flip_value_ko() {
-	new_test_ext().execute_with(|| {
-		assert_err!(Flipper::flip_value(RuntimeOrigin::signed(ALICE)), Error::NoneValue);
-	});
+	new_test_ext()
+		.execute_with(|| todo!("write a scenario that triggers an error in flip_value()"));
 }
